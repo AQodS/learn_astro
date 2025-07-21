645a30663f3e503bc45891a6f6236d61a0c244a0
AQodS <qoddri@gmail.com>
2025-07-21 23:58:13 +0700

    add rss feed

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://this-astro-web-link.com", // Replace with your actual site URL
+});
